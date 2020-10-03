447592a29b6134cc41c08685244bb8b29dceb799
grrlic <18105757+grrlic@users.noreply.github.com>
2020-10-03 22:38:25 +0700

    add base config

---
+++ configuration.py
@@ -0,0 +1,10 @@
+WIDTH = 900
+
+BLACK = (0, 0, 0) # Background
+GREY = (200, 200, 200) # Gridline
+PURPLE = (200, 100, 255) # Bound Border
+CYAN = (0, 160, 160) # Search Area
+GREEN = (0, 255, 0) # Area Border
+YELLOW = (255, 255, 0) # Trail
+ORANGE = (255, 165 ,0) # Start
+RED = (255, 0, 0) # Goal
